59f0ed1e66d3e669586b2c5ef5e4c2e89d78a234
Chris Wolfe <wolfeclw@icloud.com>
2023-09-05 14:47:20 +0800

    columbus csv fix

--- R/internal_columbus.R
+++ R/internal_columbus.R
@@ -5,43 +5,42 @@
 ## read data from csv format:
 
 columbus_csv <- function (path, tzone_gps = NULL, tzone_out = NULL) {
-  d <- suppressWarnings(readr::read_csv(path,
-                                        skip = 1, col_names = FALSE,
-                                        col_types = readr::cols(
-                                          .default = readr::col_number(),
-                                          X2 = readr::col_character(),
-                                          X4 = readr::col_character(),
-                                          X5 = readr::col_character(),
-                                          X6 = readr::col_character(),
-                                          X10 = readr::col_skip()
-                                        )
-  ))
+
+  d <- suppressWarnings(read.csv(path, header = FALSE, fill = TRUE)) %>%
+    tibble::as_tibble()
+
+  d <- d[-c(1:5), -10]
+
+  d <- d %>%
+    dplyr::mutate(dplyr::across(!V2:V6, as.numeric))
+
+
   d <- d %>%
     dplyr::mutate(
-      lat_hem = stringr::str_extract(X5, "[a-zA-Z]"),
-      lat = readr::parse_number(X5),
+      lat_hem = stringr::str_extract(V5, "[a-zA-Z]"),
+      lat = readr::parse_number(V5),
       lat = ifelse(lat_hem == "N", lat, lat * -1),
-      lon_meridian = stringr::str_extract(X6, "[a-zA-Z]"),
-      lon = readr::parse_number(X6),
+      lon_meridian = stringr::str_extract(V6, "[a-zA-Z]"),
+      lon = readr::parse_number(V6),
       lon = ifelse(lon_meridian == "W", lon * -1, lon),
-      hr = substr(X4, 1, 2),
-      min = substr(X4, 3, 4),
-      sec = substr(X4, 5, 6)
+      hr = substr(V4, 1, 2),
+      min = substr(V4, 3, 4),
+      sec = substr(V4, 5, 6)
     )
 
   d %>%
     dplyr::mutate(
-      date_utc = lubridate::ymd(X3),
+      date_utc = lubridate::ymd(V3),
       time_utc = chron::times(stringr::str_c(hr, min, sec, sep = ":")),
       Date_Time = lubridate::ymd_hms(paste(date_utc, time_utc)),
       Date_Time = lubridate::force_tzs(Date_Time, tzones = tzone_gps, tzone_out = tzone_out)
     ) %>%
     dplyr::select(Date_Time, lat, lon,
-                  gps_index = X1,
-                  tag = X2,
-                  height = X7,
-                  speed = X8,
-                  heading = X9
+                  gps_index = V1,
+                  tag = V2,
+                  height = V7,
+                  speed = V8,
+                  heading = V9
     )
 }
 
